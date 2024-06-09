f6b89a90af3ed36b66d065eba37550f6c841e5fd
Ketan Chaudhary <ketanchaudharypoonia@gmail.com>
2024-06-09 16:14:23 +0530

    Deep clone

--- src/com/Basics/CloningofObjects/Human.java
+++ src/com/Basics/CloningofObjects/Human.java
@@ -3,14 +3,29 @@ package com.Basics.CloningofObjects;
 public class Human implements Cloneable {
     int age;
     String name;
+    int[] arr;
 
     public Human(int age, String name) {
         this.age = age;
         this.name = name;
+        this.arr = new int[]{2,5,3,6,8};
     }
+//    @Override
+//    public Object clone() throws CloneNotSupportedException{
+//        return super.clone();
+//        // Shallow copy : changes in cloned object result in changes in the original object for objects
+//        // for non-primitive data type : object it will point to the original item hence changing the object in clone result in changing the original object
+//    }
+
     @Override
     public Object clone() throws CloneNotSupportedException{
-        return super.clone();
+        Human twin =(Human) super.clone(); // this is actually shallow copy
+        // Making a deep copy
+        twin.arr= new int[twin.arr.length];
+        for (int i = 0; i < twin.arr.length; i++) {
+            twin.arr[i]= this.arr[i];
+        }
+        return twin;
     }
 
 
--- src/com/Basics/CloningofObjects/Main.java
+++ src/com/Basics/CloningofObjects/Main.java
@@ -1,5 +1,7 @@
 package com.Basics.CloningofObjects;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
         Human monu= new Human(18,"Monu Chaudhary");
@@ -7,5 +9,9 @@ public class Main {
 
         Human twin= (Human) monu.clone();
         System.out.println(twin.age+" "+twin.name);
+
+        twin.arr[0]=44;
+        System.out.println(Arrays.toString(twin.arr));
+        System.out.println(Arrays.toString((monu.arr)));
     }
 }
