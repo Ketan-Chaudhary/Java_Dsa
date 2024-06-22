323c42a7d6473453ccf46a05e02b5800b2e230e1
Ketan Chaudhary <ketanchaudharypoonia@gmail.com>
2024-06-22 15:37:59 +0530

    Questions

--- src/com/Basics/LinkedList/LL.java
+++ src/com/Basics/LinkedList/LL.java
@@ -189,6 +189,45 @@ public class LL {
         return ans;
     }
 
+    // Bubble Sort
+    public void bubbleSort() {
+        bubbleSort(size - 1, 0);
+    }
+
+    private void bubbleSort(int row, int col) {
+        if (row == 0) {
+            return;
+        }
+
+        if (col < row) {
+            Node first = get(col);
+            Node second = get(col + 1);
+
+            if (first.value > second.value) {
+                // swap
+                if (first == head) {
+                    head = second;
+                    first.next = second.next;
+                    second.next = first;
+                } else if (second == tail) {
+                    Node prev = get(col - 1);
+                    prev.next = second;
+                    tail = first;
+                    first.next = null;
+                    second.next = tail;
+                } else {
+                    Node prev = get(col - 1);
+                    prev.next = second;
+                    first.next = second.next;
+                    second.next = first;
+                }
+            }
+            bubbleSort(row, col + 1);
+        } else {
+            bubbleSort(row - 1, 0);
+        }
+    }
+
     public static void main(String[] args) {
         LL first= new LL();
         LL second= new LL();
