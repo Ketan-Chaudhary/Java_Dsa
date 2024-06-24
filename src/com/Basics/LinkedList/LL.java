15cf564d91db95a7132db0c2ab006420041c30ed
Ketan Chaudhary <ketanchaudharypoonia@gmail.com>
2024-06-24 21:49:23 +0530

    Sub-list reversal

--- src/com/Basics/LinkedList/LL.java
+++ src/com/Basics/LinkedList/LL.java
@@ -260,6 +260,44 @@ public class LL {
 
     }
 
+    // Reversal a part / sub-list of list
+    public Node reverseBetween(Node head, int left, int right) {
+        if (left == right) {
+            return head;
+        }
+
+        // skip the first left-1 nodes
+        Node current = head;
+        Node prev = null;
+        for (int i = 0; current != null && i < left - 1; i++) {
+            prev = current;
+            current = current.next;
+        }
+
+        Node last = prev;
+        Node newEnd = current;
+
+        // reverse between left and right
+        Node next = current.next;
+        for (int i = 0; current != null && i < right - left + 1; i++) {
+            current.next = prev;
+            prev = current;
+            current = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+
+        if (last != null) {
+            last.next = prev;
+        } else {
+            head = prev;
+        }
+
+        newEnd.next = current;
+        return head;
+    }
+
     public static void main(String[] args) {
         LL first= new LL();
         LL second= new LL();
