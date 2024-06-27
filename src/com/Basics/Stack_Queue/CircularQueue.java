d34019777bd8c7f46f583b3fcb1e8753eb542dca
Ketan Chaudhary <ketanchaudharypoonia@gmail.com>
2024-06-27 11:36:33 +0530

    Circular Queue

---
+++ src/com/Basics/Stack_Queue/CircularQueue.java
@@ -0,0 +1,69 @@
+package com.Basics.Stack_Queue;
+
+public class CircularQueue {
+    private int[] data;
+    private static final int DEFAULT_SIZE= 10;
+    int end =0;
+    int front=0;
+    int size=0;
+
+    public CircularQueue(){
+        this(DEFAULT_SIZE);
+    }
+    public CircularQueue(int size){
+        this.data= new int[size];
+    }
+
+    public boolean insert(int item){
+        if (isFull()){
+            return false;
+        }
+        data[end++]= item;
+        end=end% data.length;
+        size++;
+        return true;
+    }
+
+    public int remove() throws Exception{
+        if (isEmpty()){
+            throw new Exception("! Queue is Empty !");
+        }
+        int removed =data[front++];
+        front=front% data.length;
+        size--;
+        return removed;
+    }
+
+    public void display(){
+        if (isEmpty()) {
+            System.out.println("Empty");
+            return;
+        }
+        int i = front;
+        do {
+            System.out.print(data[i] + " -> ");
+            i++;
+            i %= data.length;
+        } while (i != end);
+        System.out.println("END");
+    }
+
+    public int front() throws Exception{
+        if (isEmpty()){
+            throw new Exception("Queue is Empty !!");
+        }
+        return data[front];
+    }
+
+
+    public boolean isFull() {
+        if (size== data.length-1){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isEmpty(){
+        return size ==0;
+    }
+}
--- src/com/Basics/Stack_Queue/QueueMain.java
+++ src/com/Basics/Stack_Queue/QueueMain.java
@@ -2,7 +2,19 @@ package com.Basics.Stack_Queue;
 
 public class QueueMain {
     public static void main(String[] args) throws Exception{
-        CustomQueue queue= new CustomQueue(5);
+//        CustomQueue queue= new CustomQueue(5);
+//        queue.insert(3);
+//        queue.insert(23);
+//        queue.insert(53);
+//        queue.insert(35);
+//        queue.insert(65);
+//        queue.display();
+//        System.out.println( queue.front());
+//        queue.remove();
+//        queue.display();
+
+
+        CircularQueue queue= new CircularQueue(5);
         queue.insert(3);
         queue.insert(23);
         queue.insert(53);
@@ -12,5 +24,6 @@ public class QueueMain {
         System.out.println( queue.front());
         queue.remove();
         queue.display();
+
     }
 }
