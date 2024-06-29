46be8bdcaba00adfb826c0e688cae82d7c915759
Ketan Chaudhary <ketanchaudharypoonia@gmail.com>
2024-06-29 20:14:36 +0530

    valid Parenthesis

---
+++ src/com/Basics/Stack_Queue/Questions/ValidParenthesis.java
@@ -0,0 +1,36 @@
+package com.Basics.Stack_Queue.Questions;
+
+import java.util.Stack;
+
+public class ValidParenthesis {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch: s.toCharArray()){
+            if(ch == '(' || ch == '{' || ch== '['){
+                stack.push(ch);
+            }
+            else{
+                if(ch==')'){
+                    if(stack.isEmpty() || stack.pop()!= '('){
+                        return false;
+                    }
+                }
+                if(ch=='}'){
+                    if(stack.isEmpty() || stack.pop()!= '{'){
+                        return false;
+                    }
+                }
+                if(ch==']'){
+                    if(stack.isEmpty() || stack.pop()!= '['){
+                        return false;
+                    }
+                }
+            }
+
+        }
+        return stack.isEmpty();
+
+    }
+
+}
