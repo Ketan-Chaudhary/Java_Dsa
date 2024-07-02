7df2ef7cc2be2c946fe8a80e0c11fab0a933c34a
Ketan Chaudhary <ketanchaudharypoonia@gmail.com>
2024-07-02 15:17:24 +0530

    Binary Tree Implementation

---
+++ src/com/Basics/Trees/Main.java
@@ -0,0 +1,12 @@
+package com.Basics.Trees;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+         BinaryTree tree = new BinaryTree();
+         tree.populate(scanner);
+         tree.prettyDisplay();
+    }
+}
