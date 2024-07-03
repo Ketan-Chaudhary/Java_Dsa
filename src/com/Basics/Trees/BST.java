96a8d05841c73b4b4313debcf52134834335a085
Ketan Chaudhary <ketanchaudharypoonia@gmail.com>
2024-07-03 11:01:30 +0530

    Binary Search Tree

---
+++ src/com/Basics/Trees/BST.java
@@ -0,0 +1,64 @@
+package com.Basics.Trees;
+
+public class BST {
+
+    public static class Node{
+        int value;
+        Node left;
+        Node right;
+        int height;
+
+        public Node(int value){
+            this.value= value;
+        }
+    }
+
+    public BST() {
+
+    }
+
+    private Node root;
+
+    public void insert(int value){
+        root= insert(value,root);
+    }
+    private Node insert(int value, Node node){
+        if (node == null) {
+            node = new Node(value);
+            return node;
+        }
+        if (value < node.value) {
+            node.left= insert(value,node.left);
+        }
+        if (value > node.value) {
+            node.right= insert(value,node.right);
+        }
+        node.height= Math.max(height(node.left),height(node.right))+1;
+        return node;
+    }
+
+    public void populate(int[] arr){
+        for (int i=0;i<arr.length;i++){
+            this.insert(arr[i]);
+        }
+    }
+
+    public void display(){
+        display(this.root,"Root Node :");
+    }
+    private void display(Node node, String details){
+        if (node == null) {
+            return;
+        }
+        System.out.println(details+ node.value);
+        display(node.left,"left Child of :" + node.value + ":");
+        display(node.right,"right Child of :" + node.value+ ":");
+    }
+
+    public int height(Node node){
+        if (node == null) {
+            return -1;
+        }
+        return node.height;
+    }
+}
--- src/com/Basics/Trees/Main.java
+++ src/com/Basics/Trees/Main.java
@@ -4,9 +4,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-         BinaryTree tree = new BinaryTree();
-         tree.populate(scanner);
-         tree.prettyDisplay();
+//        Scanner scanner = new Scanner(System.in);
+//         BinaryTree tree = new BinaryTree();
+//         tree.populate(scanner);
+//         tree.prettyDisplay();
+
+        BST tree= new BST();
+        int[] nums= {3,5,25,234,52,1,3,6};
+        tree.populate(nums);
+        tree.display();
     }
 }
