7fe92af8ba1e1d5fcc9f5f846e3186f347d12b8e
Ketan-Chaudhary <ketanchaudharypoonia@gmail.com>
2024-08-15 16:08:51 +0530

    Symmetric Tree

---
+++ src/com/Basics/Trees/Questions/BFS/SymmetricTree.java
@@ -0,0 +1,36 @@
+package com.Basics.Trees.Questions.BFS;
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class SymmetricTree {
+//    https://leetcode.com/problems/symmetric-tree/
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+
+        while(!queue.isEmpty()){
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+
+            if(left == null && right == null){
+                continue;
+            }
+
+            if(left == null || right == null){
+                return false;
+            }
+
+            if(left.val != right.val){
+                return false;
+            }
+
+            queue.add(left.left);
+            queue.add(right.right);
+            queue.add(left.right);
+            queue.add(right.left);
+        }
+        return true;
+    }
+}
