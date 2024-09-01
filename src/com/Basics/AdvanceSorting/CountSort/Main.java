bc15961ee95e5a5f2bfa4e6b76a0d6bbaa778ec7
Ketan-Chaudhary <ketanchaudharypoonia@gmail.com>
2024-09-01 16:21:56 +0530

    Count Sort Using HashMap

--- src/com/Basics/AdvanceSorting/CountSort/Main.java
+++ src/com/Basics/AdvanceSorting/CountSort/Main.java
@@ -1,6 +1,8 @@
 package com.Basics.AdvanceSorting.CountSort;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void countSort(int[] arr){
@@ -33,9 +35,34 @@ public class Main {
         }
     }
 
+    public static void countSortHashMap(int[] arr){
+        if(arr == null || arr.length <=1) return ;
+
+        // Finding smallest and largest element
+        int min = Arrays.stream(arr).min().getAsInt();
+        int max = Arrays.stream(arr).max().getAsInt();
+
+        // Creating frequency map
+        Map<Integer,Integer> countMap = new HashMap<>();
+
+        for(int num: arr){
+            countMap.put(num , countMap.getOrDefault(num,0)+1);
+        }
+
+        // Modifying the original array
+        int index = 0;
+        for(int i = min; i <= max; i++){
+            int count = countMap.getOrDefault(i,0);
+            for(int j = 0; j< count; j++){
+                arr[index]= i;
+                index++;
+            }
+        }
+    }
+
     public static void main(String[] args){
         int [] arr = {1,3,5,3,3,6,2};
-        countSort(arr);
+        countSortHashMap(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
