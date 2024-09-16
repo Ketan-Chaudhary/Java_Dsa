feb276d3b6cdef88d7db69a6aa6efeadf5a95c22
Ketan-Chaudhary <ketanchaudharypoonia@gmail.com>
2024-09-16 10:43:27 +0530

    Two Sum IV

---
+++ src/com/Basics/Trees/QuesHashMap/TwoSumIV.java
@@ -0,0 +1,26 @@
+package com.Basics.Trees.QuesHashMap;
+import com.Basics.Trees.Questions.DFS.TreeNode;
+
+import java.util.HashSet;
+
+
+public class TwoSumIV {
+//    https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
+
+    public boolean findTarget(TreeNode root, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        return helper(root, k, set);
+    }
+
+    private boolean helper(TreeNode node, int k, HashSet<Integer> set){
+        if(node == null) return false;
+
+        if(set.contains(k - node.val)){
+            return true;
+        }
+
+        set.add(node.val);
+
+        return helper(node.left, k, set) || helper(node.right, k, set);
+    }
+}
